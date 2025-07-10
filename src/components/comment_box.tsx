749a08d84bb4048265076646f87dfd707794e6af
ingonzales_strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-10 12:25:20 +0800

    feat(UI adjustment): Creation of comment box

---
+++ src/components/comment_box.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function CommentBox({author,content,time}:{author:string,content:string,time:Date}){
+    return <div className="flex flex-col border px-5 py-2 w-120 border-blue-900 rounded-md text-wrap">
+        <div className="flex flex-row gap-2 ">
+            <h2 className="font-bold text-blue-900">{author} </h2>
+            <p>•</p>
+            <p>{time.toLocaleDateString()}</p>
+        </div>
+        <div className="text-wrap text-justify">
+            {content}
+        </div >
+    </div>
+
+}
