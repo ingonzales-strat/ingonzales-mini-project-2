bb089d269b45ab7cae0d22a65980f7ec3052c44d
ingonzales_strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-10 13:33:43 +0800

    feat(UI adjustment): Main Blog page makes uses of Blog Box

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -1,31 +1,26 @@
 
 
+import BlogBox from "@/components/blog_box";
 import {  getPublishedBlogs } from "@/lib/s_actions/blog-actions";
-import Link from "next/link";
 
 export default async function Home() {
   const data = await getPublishedBlogs();
   
-
-  console.log(data)
-
   return (
-    <div className="grid grid-rows-[20px_1fr]  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr]  p-8  pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header >
         <h1 className="text-6xl">My Blog posts</h1>
         <h2 className="text-secondary-foreground">Care to look around? :3</h2>
       </header>
-      <main>
-        <ul>
-          {data.map((blog) => (
-            <li key={blog.id}>
-              ID: {blog.id}, Title: {blog.title}
-              <Link href={`/blog/${blog.slug}`}>Link</Link>
-            </li>
-          ))}
-        </ul>
+      <div className="flex flex-col gap-5 sm:px-80">
+        
+        {data.map((blog) => (
+          <BlogBox key={blog.id} title={blog.title} description={blog.description} readMins={blog.readMinutes} publish={blog.publishedAt} slug={blog.slug}/>
+          
+        ))}
+       
  
-      </main>
+      </div>
       
 
     </div>
