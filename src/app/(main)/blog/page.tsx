087e96ee3f75a59c296782987c95c7e5e2b87f6e
ingonzales_strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-11 17:02:37 +0800

    feat: Adjustment of Blogbox and blog display responsiveness

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -13,12 +13,15 @@ export default async function BlogsPage() {
   const data = await getPublishedBlogs();
   
   return (
-    <div className="grid grid-rows-[20px_1fr]  p-8  pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header >
-        <h1 className="text-6xl">My Blog posts</h1>
+    <div className="grid grid-rows-[auto_1fr]  p-8  pb-20 gap-5  font-[family-name:var(--font-geist-sans)]">
+      <div>
+        <header >
+        <h1 className="texl-5xl md:text-6xl">My Blog posts</h1>
         <h2 className="text-secondary-foreground">Care to look around? :3</h2>
-      </header>
-      <div className="flex flex-col gap-5 sm:px-80">
+        
+        </header>
+      </div>
+      <div className="flex flex-col gap-5 p-3 px-4 sm:px-10 md:px-30 lg:px-70 xl:px-120">
         
         {data.map((blog) => (
           <BlogBox key={blog.id} id={blog.id} likes={blog.likes} title={blog.title} description={blog.description} readMins={blog.readMinutes} publish={blog.publishedAt} slug={blog.slug}/>
--- src/components/blog_box.tsx
+++ src/components/blog_box.tsx
@@ -4,10 +4,10 @@ import { LikeButton } from "./ui/blog_button";
 
 export default function BlogBox({title,publish,description, readMins, slug, id, likes}:{id:number, likes:number|null,title:string,publish:Date,description:string|null, readMins:number|null, slug:string}){
     return <div className="grid grid-cols-[auto_1fr_auto] p-5 border border-blue-900 rounded-md min-h-75 items-end bg-linear-80 from-grey-500 to-blue-300">
-        <div className="flex flex-col  text-wrap text-justify">
+        <div className="flex flex-col  text-wrap ">
             <p >{publish.toDateString()}</p>
-            <Link href={`/blog/${slug}`}><h1 className="text-5xl hover:text-blue-500">{title}</h1></Link>
-            <p>{description}</p>
+            <Link href={`/blog/${slug}`}><h1 className="text-xl sm:text-3xl lg:text-5xl hover:text-blue-500">{title}</h1></Link>
+            <p >{description}</p>
             
             {readMins && <p className="text-muted-foreground">{readMins} Minute Read</p>}
 
