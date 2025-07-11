0001e68485374e7374a670c4145e4b2cc60c27cc
ingonzales_strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-11 09:57:51 +0800

    refactor: Movement of New blog button to separate component file

--- src/app/(main)/my_blogs/page.tsx
+++ src/app/(main)/my_blogs/page.tsx
@@ -4,25 +4,34 @@ import { DeleteButton, PublishButton, UnPublishButton } from "@/components/serve
 import { Button } from "@/components/ui/button";
 import { getBlogs  } from "@/lib/s_actions/blog-actions";
 import Link from "next/link";
+import { NewBlogButton } from "@/components/ui/blog_button";
+
+
 
 export default async function Home() {
   const data = await getBlogs();
   return (
-    <div className="grid grid-rows-[20px_1fr]  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr]  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
       <header >
         <h1 className="text-6xl">Are you Niko?</h1>
         <h2 className="text-secondary-foreground">Here&apos;s your stuff</h2>
       </header>
-      <main>
-        <Button variant={"outline"} size="sm" >
-          <Link href='/my_blogs/new_blog'> New Blog</Link>
-        </Button>
+      <div className="mx-auto my-6">
+        <div className=" flex flex-col items-center py-3 gap-3">
+          <h2>Thought of Something to write?</h2>
+          <NewBlogButton/>
+
+    
+        </div>
+        <h2>Blog Post Status</h2>
         <table className="table-auto border-separate  border-spacing-5 border  items-center">
           <thead>
             <tr className="border-b border-blue-500">      
               <th>Blog ID</th>      
               <th>Title</th>      
               <th>Published At - Updated At</th> 
+              <th>Likes</th> 
+              <th>Slug</th> 
               <th>Is Published?</th> 
               <th>Actions</th>    
             </tr>
@@ -33,6 +42,8 @@ export default async function Home() {
               <td>{blog.id}</td>
               <td>{blog.title} </td>
               <td>{blog.publishedAt.toISOString()} - {blog.updatedAt.toISOString()}</td>
+              <td>{blog.likes}</td>
+              <td>{blog.slug}</td>
               <td>{blog.isPublished.toString()}</td>
               <td className="flex flex-row gap-3"> 
                 <Button>
@@ -57,7 +68,7 @@ export default async function Home() {
         </table>
      
  
-      </main>
+      </div>
       
 
     </div>
--- src/components/ui/blog_button.tsx
+++ src/components/ui/blog_button.tsx
@@ -3,7 +3,9 @@
 import { Share } from "lucide-react"
 import { Button } from "./button"
 import { toast,Toaster } from "sonner"
-//
+import {  IconCirclePlus } from "@tabler/icons-react"
+import Link from "next/link"
+
 export default function ShareButton({urlSlug}:{urlSlug:string}){
     const base="http://localhost:3000/blog/"
     const url=base+urlSlug
@@ -18,4 +20,15 @@ export default function ShareButton({urlSlug}:{urlSlug:string}){
         <Share className="size-5"/>
     </Button>
     </div>
+}
+
+export  function NewBlogButton(){
+
+    return <Link href='/my_blogs/new_blog'>
+            <Button variant="outline" size="sm" className="relative   sm:p-5 overflow-hidden text-primary before:bg-blue-500 before:absolute before:inset-0 hover:text-white transition-colors before:translate-y-[100%] hover:before:translate-y-0 before:transition-transform">
+              <span className="relative z-10  items-center gap-2 flex text-base sm:text-md md:text-lg">
+                <IconCirclePlus className="scale-100 md:scale-125 md:mr-2"/>New Blog Post
+            </span>
+            </Button>
+          </Link>
 }
